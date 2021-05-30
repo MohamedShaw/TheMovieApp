c75f51b8aab7ec83cb1bf3787399922f54364442
Mohamed Shawk <m.shawky@crocoit.com>
2021-05-30 17:14:47 +0200

    create List component to reuse it any list

---
+++ src/components/List.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {FlatList, FlatListProps} from 'react-native';
+
+interface Props<T> extends FlatListProps<T> {
+  hideIndicator?: boolean;
+}
+
+export const AppList = function <T>(props: Props<T>) {
+  const {hideIndicator, ...rest} = props;
+
+  return (
+    <FlatList
+      showsHorizontalScrollIndicator={!hideIndicator}
+      showsVerticalScrollIndicator={!hideIndicator}
+      keyExtractor={(_, index) => `${index}`}
+      onEndReachedThreshold={0.2}
+      {...rest}
+    />
+  );
+};
